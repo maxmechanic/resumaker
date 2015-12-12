20fc355fcb3679722eb2fcf96cede706363c4e6e
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-12-12 10:19:40 -0500

    Convert juice'd html from hbs to react

--- app/js/html.js
+++ app/js/html.js
@@ -1,114 +1,126 @@
-import React from 'react';
+import { map } from 'lodash'
+import React from 'react'
 
-let Html = React.createClass({
-  render() {
-    let {education, employment, name, email, skills, interests, profiles} = this.props.resume;
+const buildEducation = (education = {}) => {
+  if (!Object.keys(education).length) return null
 
-    if (name || email) {
-      var header = (
-        <div className="page-header">
-          <h1>{{name}} <small>{{email}}</small></h1>
+  return (
+    <ul className="verboseList" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '10px', 'listStyle': 'none', 'fontSize': '17px', 'paddingBottom': '10px'}}>
+      {
+        map(education, ({school, attended, degree, major}) => (
+          <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '15px'}}>
+            <h4 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '10px', 'marginBottom': '10px', 'fontSize': '18px'}}>{degree}: {major}</h4>
+            <p style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'margin': '0 0 10px'}}>{school}, {attended}</p>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+const buildProjects = (projects = []) => {
+  if (!projects.length) return null
+
+  return (
+    <ul style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '0', 'listStyle': 'none', 'fontSize': '14px', 'paddingBottom': '10px'}}>
+      {projects.map(({type, url, title}) => (
+        <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '15px'}}>
+          <b style={{'fontWeight': 'bold', 'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box'}}>{type}: </b><a href="{url}" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'color': '#428bca', 'text-decoration': 'none'}}>{title}</a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const mapEmployment = ({position, duration, projects, employer, description}) => {
+  // const projectHtml = buildProjects(projects)
+
+  return (
+    <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '15px'}}>
+      <h3 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '24px'}}>{position} <small style={{'fontSize': '14px', 'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontWeight': 'normal', 'lineHeight': '1', 'color': '#999999'}}>{duration}</small></h3>
+      <h4 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '10px', 'marginBottom': '10px', 'fontSize': '18px'}}>{employer}</h4>
+      <p style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'margin': '0 0 10px'}}>{description}</p>
+    </li>
+  )
+}
+
+const buildEmployment = (employment = {}) => {
+
+  if (!Object.keys(employment).length) return null
+  return (
+    <ul className="verboseList" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '10px', 'listStyle': 'none', 'fontSize': '17px', 'paddingBottom': '10px'}}>
+      {
+        map(employment,  mapEmployment)
+      }
+    </ul>
+  )
+
+}
+
+const buildInterests = (interests = {}) => {
+  if (!Object.keys(interests).length) return null
+
+  return (
+    <ul style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '10px', 'fontSize': '17px', 'paddingBottom': '10px'}}>
+      {map(interests, ({interest}) => (
+        <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '3px'}}>{interest}</li>
+      ))}
+    </ul>
+  )
+}
+
+const buildSkills = (skills = {}) => {
+  if (!Object.keys(skills).length) return null
+
+  return (
+    <ul style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '10px', 'fontSize': '17px', 'paddingBottom': '10px'}}>
+      {map(skills, ({skill}) => (
+        <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '3px'}}>{skill}</li>
+      ))}
+    </ul>
+  )
+}
+
+const buildProfiles = (profiles = {}) => {
+  if (!Object.keys(profiles).length) return null
+
+  return (
+    <ul className="profileList" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginTop': '0', 'marginBottom': '10px', 'fontSize': '17px', 'paddingBottom': '10px'}}>
+      {map(profiles, ({url, title}) => (
+        <li style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '3px'}}><a href="{url}" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'color': '#428bca', 'text-decoration': 'none'}}>{title}</a></li>
+      ))}
+    </ul>
+  )
+}
+
+const fullResume = ({resume}) => {
+  return (
+
+      <div className="container" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'marginRight': 'auto', 'marginLeft': 'auto', 'paddingLeft': '15px', 'paddingRight': '15px'}}>
+        <div className="page-header" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'paddingBottom': '9px', 'margin': '40px 0 20px', 'borderBottom': '1px solid #eeeeee'}}>
+          <h1 style={{'fontSize': '36px', 'margin': '0.67em 0', 'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px'}}>{resume.name} <small style={{'fontSize': '24px', 'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontWeight': 'normal', 'lineHeight': '1', 'color': '#999999'}}>{resume.email}</small></h1>
         </div>
-      );
-    }
 
-    if (education.length) {
-      var edHeader = <h2>Education</h2>;
-      var educationEls = education.map((e) => {
-        let {degree, major, school, attended} = e;
-        return (<li>
-          <h4>{degree}: {major}</h4>
-          <p>{school}, {attended}</p>
-        </li>);
-      });
+        <div className="span10" style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box'}}>
+          <h2 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '30px'}}>Education</h2>
+          {buildEducation(resume.education)}
 
-      var educationHtml =
-        (<ul className="verboseList">
-            {educationEls}
-        </ul>)
-    }
+          <h2 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '30px'}}>Employment</h2>
+          {buildEmployment(resume.employment)}
 
-    if (employment.length) {
-      var emHeader = <h2>Employment</h2>;
-      var employmentEls = employment.map((e) => {
-        let {position, duration, employer, description} = e;
-        if (e.projects) {
-          var projectsHtml = e.projects.map((p) => {
-            let {type, title, url} = p;
-
-            return (<li>
-              <b>{type}: </b><a href={url}>{title}</a>
-            </li>)
-          });
-        }
+          <h2 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '30px'}}>Interests</h2>
+          {buildInterests(resume.interests)}
 
 
-        return (<li>
-          <h3>{position} <small>{duration}</small></h3>
-          <h4>{employer}</h4>
-          <p>{description}</p>
-            <ul>
-              {projectsHtml}
-            </ul>
-        </li>);
-      });
+          <h2 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '30px'}}>Skills</h2>
+          {buildSkills(resume.skills)}
 
-      var employmentHtml =
-        (<ul className="verboseList">
-            {employmentEls}
-        </ul>)
-    }
+          <h2 style={{'WebkitBoxSizing': 'border-box', 'MozBoxSizing': 'border-box', 'boxSizing': 'border-box', 'fontFamily': 'Helvetica Neue, Helvetica, Arial, sans-serif', 'fontWeight': '500', 'lineHeight': '1.1', 'marginTop': '20px', 'marginBottom': '10px', 'fontSize': '30px'}}>Profiles</h2>
+          {buildProfiles(resume.profiles)}
+        </div>
+      </div>
 
-    if (interests.length) {
-      var inHeader =  <h2>Interests</h2>;
-      var interestsHtml = interests.map((interest) => <li>{interest}</li>);
-    }
+  )
+}
 
-    if (skills.length) {
-      var skillsHeader =  <h2>Skills</h2>;
-      var skillsHtml = skills.map((skill) => <li>{skill}</li>);
-    }
-
-    if (profiles.length) {
-      var profilesHeader =  <h2>Profiles</h2>;
-      var profilesHtml = profiles.map((profile) => {
-        let {url, title} = profile;
-        return <li><a href={url}>{title}</a></li>;
-      });
-    }
-
-    return (
-
-          <div className="container">
-            {header}
-
-            <div className="span10">
-              {edHeader}
-              {educationHtml}
-
-              {emHeader}
-              <ul className="verboseList">
-                {employmentHtml}
-              </ul>
-
-              {inHeader}
-              <ul>
-                {interestsHtml}
-              </ul>
-
-              {skillsHeader}
-              <ul>
-                {skillsHtml}
-              </ul>
-
-              {profilesHeader}
-              <ul className="profileList">
-                {profilesHtml}
-              </ul>
-            </div>
-          </div>
-    );
-  }
-});
-
-export default Html;
+export default fullResume
