a88e2718d7ca5c4f8d3017a9d3e25bc124daec48
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-12-17 23:28:41 -0500

    Style etc

--- app/js/action-creators.js
+++ app/js/action-creators.js
@@ -8,43 +8,42 @@ export function addItem(type, payload) {
   return {
     type: actions[`ADD_${type}`],
     payload
-  }
+  };
 }
 
 export function removeItem(type, payload) {
   return {
     type: actions[`REMOVE_${type}`],
     payload
-  }
+  };
 }
 
 export function beginEditItem(type, payload) {
-  console.log(type)
   return {
     type: actions[`BEGIN_EDIT_${type}`],
     payload
-  }
+  };
 }
 
 export function endEditItem(type, payload) {
   return {
     type: actions[`END_EDIT_${type}`],
     payload
-  }
+  };
 }
 
 export function editItem(type, payload) {
   return {
     type: actions[`EDIT_${type}`],
     payload
-  }
+  };
 }
 
 export function saveJSON(resume) {
   return (dispatch, getState) => {
     const blob = new Blob([JSON.stringify(resume)], {type: 'application/json'});
     saveAs(blob, 'resume.json');
-  }
+  };
 }
 
 export function saveHTML(resume) {
@@ -52,5 +51,5 @@ export function saveHTML(resume) {
     const resumeMarkup = renderToStaticMarkup(<FullResume resume={resume} />);
     const blob = new Blob([resumeMarkup], {type: 'text/html'});
     saveAs(blob, 'resume.html');
-  }
+  };
 }
