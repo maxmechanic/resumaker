3cab14ca5b2f1847202ceab3057f85751352d757
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-12-19 01:18:43 -0500

    Fix prod setup

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,21 @@
 var webpack = require('webpack')
 var defaults = require('./webpack.defaults')
 
-var definePlugin = new webpack.DefinePlugin({
-  __DEV__: process.env.NODE_ENV !== 'production,
-});
-
 var prodConfig = {
   plugins: [
-     new webpack.optimize.OccurenceOrderPlugin(),
-     new webpack.DefinePlugin({
-       process.env: {
-         NODE_ENV: process.env.NODE_ENV
-       },
-       __DEV__: process.env.NODE_ENV !== 'production'
-     }),
-     new webpack.optimize.UglifyJsPlugin({
-       compressor: {
-         warnings: false
-       }
-     })
-   ]
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      },
+      __DEV__: process.env.NODE_ENV !== 'production'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
 }
 
 module.exports = Object.assign({}, defaults, prodConfig)
